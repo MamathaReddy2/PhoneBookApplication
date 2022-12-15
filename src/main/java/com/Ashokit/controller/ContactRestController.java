ab8d8b11736882bd1e6a47857a1b96c99bbdc109
MamathaReddy2 <koppula.mamatha@gmail.com>
2022-12-15 10:34:31 -0600

    initial commit

--- src/main/java/com/Ashokit/controller/ContactRestController.java
+++ src/main/java/com/Ashokit/controller/ContactRestController.java
@@ -3,6 +3,7 @@ package com.Ashokit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.Ashokit.binding.Contact;
 import com.Ashokit.service.ContactService;
 
 @RestController
+@CrossOrigin
 public class ContactRestController {
 	@Autowired
 	private ContactService service;
